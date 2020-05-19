6c7d392c9446fb54bb31d10c16955487503cc48f
Eduardo Gonzalez <edgonzalezhernandez9@gmail.com>
2020-05-19 12:31:26 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,7 +2,7 @@ def introduction(name)
   puts "Hi, my name is #{name}"
 end
 
-introdcution("Ed")
+introduction ("Ed")
 
 def introduction_with_language (name, language)
   puts "hi, my name is #{name} and i am learning to program in #{language}"
