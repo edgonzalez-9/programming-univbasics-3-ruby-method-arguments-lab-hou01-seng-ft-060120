461da6a199bd24221fed98be67f8f1c2dbb59fb6
Eduardo Gonzalez <edgonzalezhernandez9@gmail.com>
2020-05-19 12:33:15 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,15 +2,19 @@ def introduction(name)
   puts "Hi, my name is #{name}"
 end
 
-introduction ("Ed")
+introduction("Ed")
 
-def introduction_with_language (name, language)
+def introduction_with_language(name, language)
   puts "hi, my name is #{name} and i am learning to program in #{language}"
 end
 
-introduction_with_language ("Ed", "ruby")
+introduction_with_language("Ed", "ruby")
 
+def introduction_with_language_optional(name, language = ruby)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}"
+end
 
+introduction_with_language_optional ("Ed")
 
 
 
